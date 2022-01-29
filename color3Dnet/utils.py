fed906ef620ab1415b1ecbe2b2728aa8df7e5539
Sebastian-Landl <43909009+Sebastian-Landl@users.noreply.github.com>
2022-01-29 12:00:18 +0100

    Fix several code issues (#1)

--- color3Dnet/utils.py
+++ color3Dnet/utils.py
@@ -20,13 +20,10 @@ def check_gpu():
 
     gpus = tf.config.experimental.list_physical_devices('GPU')
     if gpus:
-        # Restrict TensorFlow to only use the first GPU
         try:
-            tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
             logical_gpus = tf.config.experimental.list_logical_devices('GPU')
             print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
         except RuntimeError as e:
-            # Visible devices must be set before GPUs have been initialized
             print(e)
 
 
